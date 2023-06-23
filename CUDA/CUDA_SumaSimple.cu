7207d8b6f86e9cecaf0bd28ed6eb40f6ae2a3ee4
Juan Camilo Guerrero Ortega <guerreroortegajuancamilo@gmail.com>
2023-06-23 16:39:45 -0500

    UPDATE
    SUMASIMPLE

--- CUDA/CUDA_SumaSimple.cu
+++ CUDA/CUDA_SumaSimple.cu
@@ -1,52 +1,38 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 
-#define BLOCK_SIZE 256
-
-__device__ long long int atomicAddLL(long long int* address, long long int val) {
-    unsigned long long int* address_as_ull = (unsigned long long int*)address;
-    unsigned long long int old = *address_as_ull, assumed;
-
-    do {
-        assumed = old;
-        old = atomicCAS(address_as_ull, assumed, static_cast<unsigned long long int>(val + assumed));
-    } while (assumed != old);
-
-    return old;
-}
-
-__global__ void sumKernel(int num, long long int* result) {
+__global__ void sumKernel(int num, int* result) {
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
     int stride = blockDim.x * gridDim.x;
 
-    long long int sum = 0;
+    int sum = 0;
 
     for (int count = idx + 1; count <= num; count += stride) {
         sum += count;
     }
 
-    atomicAddLL(result, sum);
+    atomicAdd(result, sum);
 }
 
 int main() {
-    int num;
-    long long int sum = 0;
-    long long int* dev_result;
+    int num, sum = 0;
+    int* dev_result;
 
     printf("Enter a positive integer: ");
     scanf("%d", &num);
 
-    cudaMalloc((void**)&dev_result, sizeof(long long int));
-    cudaMemcpy(dev_result, &sum, sizeof(long long int), cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&dev_result, sizeof(int));
+    cudaMemcpy(dev_result, &sum, sizeof(int), cudaMemcpyHostToDevice);
 
-    int numBlocks = (num + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    int blockSize = 256;  // Puedes ajustar el tamaño del bloque según tus necesidades
+    int numBlocks = (num + blockSize - 1) / blockSize;
 
-    sumKernel<<<numBlocks, BLOCK_SIZE>>>(num, dev_result);
+    sumKernel<<<numBlocks, blockSize>>>(num, dev_result);
     cudaDeviceSynchronize();
 
-    cudaMemcpy(&sum, dev_result, sizeof(long long int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&sum, dev_result, sizeof(int), cudaMemcpyDeviceToHost);
 
-    printf("\nSum = %lld\n", sum);
+    printf("\nSum = %d\n", sum);
 
     cudaFree(dev_result);
 
