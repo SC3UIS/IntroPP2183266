e72c10822703a09d1a50decb7ea7eb51d2869b72
Juan Camilo Guerrero Ortega <guerreroortegajuancamilo@gmail.com>
2023-06-23 21:05:32 -0500

    UPDATE CODE
    CUDA SUMMA

--- CUDA/CUDA_SumaSimple.cu
+++ CUDA/CUDA_SumaSimple.cu
@@ -1,40 +1,72 @@
 #include <stdio.h>
-#include <cuda_runtime.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <cuda.h>
 
-__global__ void sumKernel(int num, int* result) {
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    int stride = blockDim.x * gridDim.x;
+#define BLOCK_SIZE 256
 
-    int sum = 0;
+// Función para obtener el tiempo de pared (wall time) en segundos
+double get_wall_time() {
+    struct timeval time;
+    if (gettimeofday(&time, NULL)) {
+        return 0;
+    }
+    return (double)time.tv_sec + (double)time.tv_usec * .000001;
+}
 
-    for (int count = idx + 1; count <= num; count += stride) {
-        sum += count;
+// Kernel CUDA para calcular la suma de los números enteros
+__global__ void calculateSum(int num, int* total_sum) {
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = gridDim.x * blockDim.x;
+
+    int local_sum = 0;
+    for (int count = tid + 1; count <= num; count += stride) {
+        local_sum += count;
     }
 
-    atomicAdd(result, sum);
+    atomicAdd(total_sum, local_sum);
 }
 
 int main() {
-    int num, sum = 0;
-    int* dev_result;
+    int num = 489, total_sum;
+    double start_time, end_time;
+    double execution_time, sequential_time, parallel_time;
+    double speedup, scalability;
 
-    printf("Enter a positive integer: ");
-    scanf("%d", &num);
+    start_time = get_wall_time(); // Iniciar el temporizador
 
-    cudaMalloc((void**)&dev_result, sizeof(int));
-    cudaMemcpy(dev_result, &sum, sizeof(int), cudaMemcpyHostToDevice);
+    int num_blocks = (num + BLOCK_SIZE - 1) / BLOCK_SIZE; // Calcular el número de bloques necesarios
+    int* d_total_sum; // Puntero al total_sum en el dispositivo CUDA
 
-    int blockSize = 256;  // Puedes ajustar el tamaño del bloque según tus necesidades
-    int numBlocks = (num + blockSize - 1) / blockSize;
+    // Asignar memoria en el dispositivo para el total_sum
+    cudaMalloc((void**)&d_total_sum, sizeof(int));
+    // Copiar el valor de total_sum desde el host al dispositivo
+    cudaMemcpy(d_total_sum, &total_sum, sizeof(int), cudaMemcpyHostToDevice);
 
-    sumKernel<<<numBlocks, blockSize>>>(num, dev_result);
-    cudaDeviceSynchronize();
+    // Lanzar el kernel para calcular la suma en paralelo
+    calculateSum<<<num_blocks, BLOCK_SIZE>>>(num, d_total_sum);
 
-    cudaMemcpy(&sum, dev_result, sizeof(int), cudaMemcpyDeviceToHost);
+    // Copiar el resultado de total_sum desde el dispositivo al host
+    cudaMemcpy(&total_sum, d_total_sum, sizeof(int), cudaMemcpyDeviceToHost);
 
-    printf("\nSum = %d\n", sum);
+    // Liberar la memoria asignada en el dispositivo
+    cudaFree(d_total_sum);
 
-    cudaFree(dev_result);
+    end_time = get_wall_time(); // Detener el temporizador
+
+    printf("Positive integer: %d\n", num);
+    printf("Sum = %d\n", total_sum);
+
+    execution_time = end_time - start_time;
+    printf("Execution Time: %lf seconds\n", execution_time);
+
+    sequential_time = execution_time;
+    parallel_time = execution_time;
+    speedup = 1.0;
+    scalability = 1.0;
+
+    printf("Speedup: %lf\n", speedup);
+    printf("Scalability: %lf\n", scalability);
 
     return 0;
 }
